3c5ceaddbd2be157e41e61d38ee36b66c0235bc3
Adam Gyulavari <adam.gyulavari@lab.coop>
2018-03-22 13:22:12 +0100

    add user id to successful response of user creation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,7 +2,7 @@ class UserController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-      render json: {status: 'ok'}
+      render json: {status: 'ok', user_id: user.id}
     else
       render json: {errors: user.errors}
     end
