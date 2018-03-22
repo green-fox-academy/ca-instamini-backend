17821e52b3f6c9d29e0b8baa0e6675bf9e204781
Adam Gyulavari <adam.gyulavari@lab.coop>
2018-03-22 12:59:05 +0100

    implement user creation

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,16 @@
 class UserController < ApplicationController
   def create
+    user = User.new(user_params)
+    if user.save
+      render json: {status: 'ok'}
+    else
+      render json: {errors: user.errors}
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 end
